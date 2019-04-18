bef18b8775e83df621504697ea9f2625f6917b39
Madeline Rombes <madelinerombes@gmail.com>
2019-04-18 17:42:51 -0400

    fix header

--- mari-vanna-website/src/App.js
+++ mari-vanna-website/src/App.js
@@ -1,34 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Careers from './components/Careers'
-import Drinks from './components/Drinks'
-import Info from './components/Info'
 import Header from './components/Header.js'
-import Dinner from './components/Dinner.js'
-import Dessert from './components/Dessert';
-import IconBar from './components/IconBar.js'
-import MenuNav from './components/MenuNav.js'
-import Footer from './components/Footer.js'
-import Lunch from './components/Lunch';
 import Gallery from './components/Gallery';
-
+import IconBar from './components/IconBar.js'
+import Info from './components/Info'
+import Footer from './components/Footer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
         <Header />
         <Gallery/>
-        <Info/>
         <IconBar />
-        <MenuNav />
-        <Lunch/>
-        <Dinner />
-        <Drinks />
-        {/* <Dessert/> */}
-        {/* <Careers /> */}
-        {/* <Footer /> */}
+        <Info/>
+        <Footer />
       </div>
     );
   }
