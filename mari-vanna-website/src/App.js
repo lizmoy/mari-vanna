2674c59219b7ca1891d885c683faa2564230623f
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-04-18 11:25:17 -0400

    test Info component in App.js

--- mari-vanna-website/src/App.js
+++ mari-vanna-website/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Info from './components/Info'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Info/>
       </div>
     );
   }
