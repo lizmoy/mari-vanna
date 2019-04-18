632ae1d7edc25725eaec025d2ad688220693396a
Daniel Kuang <danielkuang@ool-ad02001b.dyn.optonline.net>
2019-04-18 12:41:32 -0400

    Created footer

--- mari-vanna-website/src/App.js
+++ mari-vanna-website/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Footer from './components/Footer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <Footer/>
       </div>
     );
   }
