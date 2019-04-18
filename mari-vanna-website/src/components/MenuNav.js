5bd452826242b850355bf0f88ea3dd5ec3310c37
Madeline Rombes <madelinerombes@gmail.com>
2019-04-18 16:16:43 -0400

    done with menunav

--- mari-vanna-website/src/components/MenuNav.js
+++ mari-vanna-website/src/components/MenuNav.js
@@ -9,16 +9,14 @@ class MenuNav extends Component {
   render() {
     return (
       <div className="menunav">
-        <div className="menunav__text">
-            <a className="menunav__lunch" href='#'>LUNCH</a>
-            <img id="menunav__img--lunch" src={Lunch} />
-            <a className="menunav__dinner" href='#'>DINNER</a>
-            <img id="menunav__img--dinner" src={Dinner} />
-            <a className="menunav__dessert" href='#'>DESSERT</a>
-            <img id="menunav__img--dessert" src={Dessert} />
-            <a className="menunav__drinks" href='#'>DRINKS</a>
-            <img id="menunav__img--drinks" src={Drinks} />
-        </div>
+        <a className="menunav__lunch" href='#'>LUNCH</a>
+        <img id="menunav__img--lunch" src={Lunch} />
+        <a className="menunav__dinner" href='#'>DINNER</a>
+        <img id="menunav__img--dinner" src={Dinner} />
+        <a className="menunav__dessert" href='#'>DESSERT</a>
+        <img id="menunav__img--dessert" src={Dessert} />
+        <a className="menunav__drinks" href='#'>DRINKS</a>
+        <img id="menunav__img--drinks" src={Drinks} />
       </div>
     )
   }
