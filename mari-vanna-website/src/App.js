9cc6cf6c6e377095954c26b8a582f3b390b4e673
galbertoneil <goneilp@gmail.com>
2019-04-18 14:26:24 -0400

    partial styling drinks component

--- mari-vanna-website/src/App.js
+++ mari-vanna-website/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Careers from './components/Careers'
+import Drinks from './components/Drinks'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Careers />
+        {/* <Careers /> */}
+        <Drinks />
       </div>
     );
   }
---
+++ mari-vanna-website/src/components/Drinks.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import drinkBanner from '../images/cocktails_banner.jpg'
+
+export class Drinks extends Component {
+  render() {
+    return (
+        <div>
+            <div className='drinks-mainImage'>
+                <img className='drinks-image' src={drinkBanner} alt='drinksPhoto' />
+            </div>
+            <div className='drinks--mainContainer'>
+
+                <h1 className='drinks--mainTitle'>DRINKS</h1>
+
+                <div className='drinks--alcoholicDrinks'>
+
+                    <h4 className='drinks--vodkaMenu-'>HOUSE INFUSED VODKA</h4>
+                    <p classname='drinks--vodkaDescription'>Proudly Infused with Russian Standard Vodka</p>
+                    <ul className='drinks--vodkaFlavors1'>
+                        <li>Apricot</li>
+                        <li>Black Currant</li>
+                        <li>Cherry</li>
+                        <li>Cranberry</li>
+                        <li>Cucumber and Dill</li>
+                    </ul>
+                    <ul className='drinks--vodkaFlavors2'>
+                        <li>Horseradish</li>
+                        <li>Pear and Vanilla</li>
+                        <li>Pepper and Honey</li>
+                        <li>Pickle and Garlic</li>
+                        <li>Strawberry</li>
+                    </ul>
+                    <ul className='drinks-vodkaPrices'>
+                        <li>Carafe 300ml $60</li>
+                        <li>Carafe 500ml $100</li>
+                        <li>Shot $11</li>
+                    </ul>
+                    <ul className='drinks--vodkaFlights'>
+                        <li>Flight of 5 Shots $50</li>
+                        <li>Flight of 10 Shots $90</li>
+                    </ul>
+
+                </div>
+
+                <div className='drinks--nonAlcoholicDrinks'>
+                    <ul>
+                        <li>Glass of Tea $4</li>
+                        <li>Cranberry Mors Homemade Berry Punch $5</li>
+                        <li>Kvas Russian style Root Beer $5</li>
+                        <li>Evian water $12</li>
+                        <li>Badoit Sparkling Water $12</li>
+                        <li>Elderflower lemonade $6</li>
+                        <li>Coffee $5</li>
+                        <li>Espresso $5</li>
+                        <li>Double Espresso $6</li>
+                        <li>Cappuccino $6</li>
+                        <li>Grand Tea Pot  $14</li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+        </div>
+    )
+  }
+}
+
+export default Drinks
