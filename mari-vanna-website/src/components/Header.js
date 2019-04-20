bf41c8749e3658a0eef5201ea2de24b3767c0840
Madeline Rombes <madelinerombes@gmail.com>
2019-04-20 13:52:15 -0400

    hamburger done

--- mari-vanna-website/src/components/Header.js
+++ mari-vanna-website/src/components/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import { HashLink } from 'react-router-hash-link';
+import Hamburger from '../images/hamburger.png'
 
 
-const Header = () => {
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false
+    }
+    this.toggleMobileNav = this.toggleMobileNav.bind(this)
+  }
+
+  toggleMobileNav() {
+    const mobileState = this.state.active
+    this.setState({ active: !mobileState })
+  }
+
+  render() {
     return (
         <div className="header">
           <div className="header__title">MARI VANNA</div>
@@ -13,8 +28,20 @@ const Header = () => {
             <HashLink className="header__atag" to="/#menu">MENU</HashLink>
             <a className="header__atag" target='blank' href='https://www.opentable.com/r/mari-vanna-new-york'>RESERVATIONS</a>
           </nav>
+          <div className="header__mobilenav">
+            <img className="hamburger" src={Hamburger} onClick={this.toggleMobileNav}></img>
+            <div 
+              className={this.state.active ? "header_nav--content-on" : "header_nav--content-off"}>
+
+              <Link className="header__atag--mobile-home" to="/">HOME</Link>
+              <HashLink className="header__atag--mobile-about" to="/#about">ABOUT</HashLink>
+              <HashLink className="header__atag--mobile-menu" to="/#menu">MENU</HashLink>
+              <a className="header__atag--mobile-reservations" target='blank' href='https://www.opentable.com/r/mari-vanna-new-york'>RESERVATIONS</a>      
+            </div> 
+          </div>
         </div>
     )
+}
 }
 
 export default Header
